0db689adba84b96f2c43b81c76eec53c5c1915a0
hyoryeong250 <hyoryeong@dgu.ac.kr>
2026-08-17 17:15:22 +0900

    feat: 랜딩페이지에서 이미지 60개 preload

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,29 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import MobileLayout from "../components/MobileLayout/MobileLayout";
 import * as S from "./LandingPage.styled";
 import { BgmManager } from "../components/SoundButton";
 
+import { shelfData } from "../components/Shelf/ShelfData";
+
 function LandingPage() {
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const activeZones = [1, 2, 3, 4, 5, 6, 7];
+
+        const imageUrls = activeZones
+            .flatMap((zone) => shelfData[zone] ?? [])
+            .map((product) => product.imageUrl)
+            .filter(Boolean);
+
+        imageUrls.forEach((src) => {
+            const img = new Image();
+            img.src = src;
+        });
+
+        console.log(`${imageUrls.length}개 상품 이미지 preload 시작`);
+    }, []);
 
     return (
         <MobileLayout showHeader={false}>
@@ -16,15 +35,16 @@ function LandingPage() {
                         <S.LogoText>O</S.LogoText>
                     </S.Logo>
 
-
                     <S.ButtonContainer>
-
                         <S.EnterButton
                             type="button"
                             onClick={() => {
-                                BgmManager.play().catch((err) => console.log("음악 재생 실패:", err));
-                                navigate("/home")}
-                            }
+                                BgmManager.play().catch((err) =>
+                                    console.log("음악 재생 실패:", err)
+                                );
+
+                                navigate("/home");
+                            }}
                         >
                             입장하기
                         </S.EnterButton>
