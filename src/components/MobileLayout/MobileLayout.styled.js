d5469f506f7f3b83dc83cd516ce00a24cda7f3bb
feseungwon <aza0909@dgu.ac.kr>
2026-08-19 19:54:54 +0900

    fix : mobile layout

--- src/components/MobileLayout/MobileLayout.styled.js
+++ src/components/MobileLayout/MobileLayout.styled.js
@@ -5,6 +5,7 @@ export const PageBackground = styled.div`
   justify-content: center;
 
   width: 100%;
+  min-height: 100vh;
   background-color: #eeeeee;
 `;
 
@@ -15,14 +16,25 @@ export const MobileContainer = styled.div`
   max-width: 402px;
 
   background-color: #ffffff;
+
+  /* 휴대폰 화면에서만 전체 너비 사용 */
+  @media (max-width: 600px) {
+    width: 100vw;
+    max-width: none;
+  }
 `;
 
 export const MainContent = styled.main`
   min-height: ${({ $showHeader }) =>
-    $showHeader ? "calc(100dvh - 103px)" : "100dvh"};
+    $showHeader ? "calc(100vh - 103px)" : "100vh"};
 
   background-color: ${({ $showHeader }) =>
     $showHeader ? "#ffffff" : "#222222"};
 
   overflow: hidden;
+
+  @media (max-width: 600px) {
+    min-height: ${({ $showHeader }) =>
+      $showHeader ? "calc(100dvh - 103px)" : "100dvh"};
+  }
 `;
